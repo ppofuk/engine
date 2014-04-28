0d1b415ae8fd62910011f3801ad5b3badbfd91ea
Petar Pofuk <ppofuk@gmail.com>
2014-04-28 23:22:06 +0200

    New buffer implementation. Methods CreateElementBuffer and CreateVertexBuffer are removed from class. See gl-buffer.h.

--- library/gl/gl-buffer.h
+++ library/gl/gl-buffer.h
@@ -9,50 +9,151 @@
 
 namespace render {
 
-template <typename Type>
-class GLBuffer {
+class GLBufferBase {
  public:
-  GLBuffer() : data_(0), buffer_(0) {};
+  GLBufferBase(GLenum usage, void* data, size_t size);
+  GLBufferBase();
 
-  void set_data(Type* data) { data_ = data; }
-  Type* get_data() const { return data_; }
-  void set_size(size_t size) { size_ = size; }
+  GLenum get_type() const { return type_; }
+  GLenum get_usage() const { return usage_; }
+  void* get_data() const { return data_; }
   size_t get_size() const { return size_; }
   GLuint get_buffer() const { return buffer_; }
-  GLenum get_type() const { return type_; }
 
-  void Create(GLenum type) {
-    glGenBuffers(1, &buffer_);
-    glBindBuffer(type, buffer_);
-    glBufferData(type,
-                 size_ * sizeof(Type),
-                 data_,
-                 GL_STATIC_DRAW);
-    type_ = type;
-  }
+  void Create(GLenum type);
+  void Create(GLenum type, GLenum usage, void* data, size_t size);
 
-  void CreateVertexBuffer(Type* data, size_t size) {
-    set_data(data);
-    set_size(size);
-    Create(GL_ARRAY_BUFFER);
-  }
+  // Destroy buffer GL object.
+  void Destroy();
 
-  void CreateElementBuffer(Type* data, size_t size) {
-    set_data(data);
-    set_size(size);
-    Create(GL_ELEMENT_ARRAY_BUFFER);
-  }
+  // Bind the current buffer.
+  void Bind();
 
-  void Bind() {
-    glBindBuffer(type_, buffer_);
+  // Two buffer objects are logically the same if they have the same GL object
+  // number.
+  bool operator==(const GLBufferBase& rhs) {
+    return (buffer_ == rhs.get_buffer());
   }
 
  private:
-  Type* data_;
+  void* data_;
   size_t size_;
-  GLuint buffer_;
   GLenum type_;
+  GLenum usage_;
+  GLuint buffer_;
 };
-} //  namespace render
 
-#endif
+template <typename T>
+class GLBuffer : public GLBufferBase {};
+
+template <>
+class GLBuffer<void> {
+ public:
+  // Destroy current buffer.
+  void Destroy() { buffer_base_.Destroy(); }
+
+  // Bind the current buffer.
+  void Bind() { buffer_base_.Bind(); }
+
+  // Create a new buffer of void* type. |size| is the memory size in bytes
+  // of pointed array |data|.
+  void Create(GLenum type, GLenum usage, void* data, size_t size) {
+    buffer_base_.Create(type, usage, data, size);
+  }
+
+  const GLBufferBase& buffer_base() const { return buffer_base_; }
+
+  template <typename U>
+  bool Equal(const GLBuffer<U>& buffer) {
+    return (buffer_base_ == buffer.buffer_base());
+  }
+
+  // Returns the actual size of pointed array in bytes.
+  virtual size_t size() const { return buffer_base_.get_size(); }
+
+  virtual inline void* get_offset(size_t offset) { return (void*)(offset); }
+
+ protected:
+  GLBufferBase buffer_base_;
+};
+
+template <>
+class GLBuffer<GLfloat> : public GLBuffer<void> {
+  // Create a new buffer that holds GLfloat type.
+  // |size| is the number of elements, not the memory size in bytes.
+  void Create(GLenum type, GLenum usage, GLfloat* data, size_t size) {
+    buffer_base_.Create(
+        type, usage, static_cast<void*>(data), size * sizeof(GLfloat));
+  }
+
+  // Returns the number of elements in pointed array, not the size of array.
+  // If you need to get the actual size you can use |buffer_base().get_size()|,
+  // or multiply the returned value from this method by sizeof(GLfloat).
+  size_t size() const { return buffer_base_.get_size() / sizeof(GLfloat); }
+
+  inline void* get_offset(size_t offset) {
+    return (void*)(offset * sizeof(GLfloat));
+  }
+};
+
+template <>
+class GLBuffer<GLint> : public GLBuffer<void> {
+  // Create a new buffer that holds GLint type.
+  // |size| is the number of elements, not the memory size in bytes.
+  void Create(GLenum type, GLenum usage, GLint* data, size_t size) {
+    buffer_base_.Create(
+        type, usage, static_cast<void*>(data), size * sizeof(GLint));
+  }
+
+  // Returns the number of elements in pointed array, not the size of array.
+  // If you need to get the actual size you can use |buffer_base().get_size()|,
+  // or multiply the returned value from this method by sizeof(GLint).
+  size_t size() const { return buffer_base_.get_size() / sizeof(GLint); }
+
+  inline void* get_offset(size_t offset) {
+    return (void*)(offset * sizeof(GLint));
+  }
+};
+
+template <>
+class GLBuffer<GLushort> : public GLBuffer<void> {
+  // Create a new buffer that holds GLushort type.
+  // |size| is the number of elements, not the memory size in bytes.
+  void Create(GLenum type, GLenum usage, GLushort* data, size_t size) {
+    buffer_base_.Create(
+        type, usage, static_cast<void*>(data), size * sizeof(GLushort));
+  }
+
+  // Returns the number of elements in pointed array, not the size of array.
+  // If you need to get the actual size you can use |buffer_base().get_size()|,
+  // or multiply the returned value from this method by sizeof(GLushort).
+  size_t size() const { return buffer_base_.get_size() / sizeof(GLushort); }
+
+  inline void* get_offset(size_t offset) {
+    return (void*)(offset * sizeof(GLushort));
+  }
+};
+
+
+// Helper functions that call GLBuffer<T>::Create(type, usage, data, size)
+// method with type value correlated to function name (vertex buffer, element
+// buffer)
+template <typename T>
+void CreateVertexBuffer(const GLBuffer<T>& buffer,
+                        GLenum usage,
+                        T* data,
+                        size_t size) {
+  buffer.Create(GL_ARRAY_BUFFER, usage, data, size);
+}
+
+template <typename T>
+void CreateElementBuffer(const GLBuffer<T>& buffer,
+                         GLenum usage,
+                         T* data,
+                         size_t size) {
+  buffer.Create(GL_ELEMENT_ARRAY_BUFFER, usage, data, size);
+}
+
+}  //  namespace render
+
+#endif  // OBSIDIAN_GL_BUFFER_H_
