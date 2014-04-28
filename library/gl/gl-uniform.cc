1b9798e839bcaaeed09ff3e105fbd179a6ed4ba6
Petar Pofuk <ppofuk@gmail.com>
2014-04-28 23:36:50 +0200

    GLUnifrom is no logner a template class.

--- library/gl/gl-uniform.cc
+++ library/gl/gl-uniform.cc
@@ -7,17 +7,29 @@
 #include <assert.h>
 
 namespace render {
-namespace intern {
 
-GLUniformBase::GLUniformBase() : location_(-1), program_(0) {}
+GLUniform::GLUniform() : location_(-1), program_(0) {}
 
-void GLUniformBase::Locate(const render::GLProgram& program,
-                           const char* uniform_name) {
+void GLUniform::Locate(const render::GLProgram& program,
+                       const char* uniform_name) {
   program_ = program.get_program();
   assert(strlen(uniform_name) < kUniformNameSize - 1);
   memcpy(uniform_name_, uniform_name, kUniformNameSize);
   location_ = glGetUniformLocation(program_, uniform_name_);
 }
 
-}  // namespace intern
+void GLUniform::Pass(GLfloat value) { glUniform1f(location_, value); }
+
+void GLUniform::Pass(GLint value) { glUniform1i(location_, value); }
+
+void GLUniform::Pass(GLuint value) { glUniform1ui(location_, value); }
+
+void GLUniform::Pass(GLfloat* values, size_t count) {
+  glUniform1fv(location_, count, values);
+}
+
+void GLUniform::Pass(GLint* values, size_t count) {
+  glUniform1iv(location_, count, values);
+}
+
 }  // namespace render
--- library/gl/gl-uniform.h
+++ library/gl/gl-uniform.h
@@ -11,11 +11,9 @@
 
 namespace render {
 
-namespace intern {
-
-class GLUniformBase {
+class GLUniform {
  public:
-  GLUniformBase();
+  GLUniform();
   // Locate a new or existing uniform variable of uniform_name in program.
   // This method should be always called.
   void Locate(const render::GLProgram& program, const char* uniform_name);
@@ -24,68 +22,17 @@ class GLUniformBase {
   void set_location(const GLint location) { location_ = location; }
   const char* get_uniform_name() const { return uniform_name_; }
 
-  virtual ~GLUniformBase() {}
-
   // Pass method is a wraper around glUniform... function.
-
+  void Pass(GLfloat value);
+  void Pass(GLint value);
+  void Pass(GLuint value);
+  void Pass(GLfloat* values, size_t count);
+  void Pass(GLint* values, size_t count);
 
  protected:
   char uniform_name_[kUniformNameSize];
   GLint location_;
   GLuint program_;
-};
-
-}  // namespace intern
-
-template <typename T>
-class GLUniform : public intern::GLUniformBase {
- public:
-  typedef void Type;
-  GLUniform() : intern::GLUniformBase() {}
-};
-
-template <>
-class GLUniform<GLfloat> : public intern::GLUniformBase {
- public:
-  typedef GLfloat Type;
-  GLUniform() : intern::GLUniformBase() {}
-  void Pass(GLfloat value) { glUniform1f(location_, value); }
-};
-
-template <>
-class GLUniform<GLint> : public intern::GLUniformBase {
- public:
-  typedef GLint Type;
-  GLUniform() : intern::GLUniformBase() {}
-  void Pass(GLint value) { glUniform1i(location_, value); }
-};
-
-template <>
-class GLUniform<GLuint> : public intern::GLUniformBase {
- public:
-  typedef GLint Type;
-  GLUniform() : intern::GLUniformBase() {}
-  void Pass(GLuint value) { glUniform1ui(location_, value); }
-};
-
-template <>
-class GLUniform<GLfloat*> : public intern::GLUniformBase {
- public:
-  typedef GLfloat* Type;
-  GLUniform() : intern::GLUniformBase() {}
-  void Pass(GLfloat* values, size_t count) {
-    glUniform1fv(location_, count, values);
-  }
-};
-
-template <>
-class GLUniform<GLint*> : public intern::GLUniformBase {
- public:
-  typedef GLint* Type;
-  GLUniform() : intern::GLUniformBase() {}
-  void Pass(GLint* values, size_t count) {
-    glUniform1iv(location_, count, values);
-  }
 };
 
 }  //  namespace render
