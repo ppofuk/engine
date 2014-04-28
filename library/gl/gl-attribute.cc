894e828563cada1e839f7a3842f2eb3e50cc904c
Petar Pofuk <ppofuk@gmail.com>
2014-04-28 23:20:41 +0200

    New prototype and implementation of GLAttribute.

---
+++ library/gl/gl-attribute.cc
@@ -0,0 +1,23 @@
+// Copyright (C) 2014 Petar Pofuk. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+#include "gl-attribute.h"
+
+#include <string.h>
+#include <assert.h>
+
+namespace render {
+namespace intern {
+
+GLAttributeBase::GLAttributeBase() : location_(-1), program_(0) {}
+
+void GLAttributeBase::Locate(const render::GLProgram& program,
+                             const char* attrib_name) {
+  program_ = program.get_program();
+  assert(strlen(attrib_name) < kAttribNameSize - 1);
+  memcpy(attrib_name_, attrib_name, kAttribNameSize);
+  location_ = glGetAttribLocation(program_, attrib_name_);
+}
+
+}  // namespace intern
+}  // namespace render
