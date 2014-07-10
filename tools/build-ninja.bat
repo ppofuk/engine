2b0fd52432a73fd918b7bcd677515b245ce9878b
ppofuk <ppofuk@gmail.com>
2014-07-10 22:34:58 +0200

    Use it manauly.

--- tools/build-ninja.bat
+++
@@ -1,8 +0,0 @@
-@rem Copyright (C) 2013 Petar Pofuk. All rights reserved.
-@rem Use of this source code is governed by a BSD-style license that can be
-@rem found in the LICENSE file.
-
-call vsvars32.bat
-cd ninja
-@python "%~dp0bootstrap.py" %*
-pause
