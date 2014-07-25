aa49e752fedf53b83ef5fba8fd322ddf69bba89e
ppofuk <ppofuk@gmail.com>
2014-07-25 02:44:52 +0200

    Test new window and context concept.

--- app/main-msvc.cc
+++ app/main-msvc.cc
@@ -4,42 +4,58 @@
 #include "GL/glew.h"
 
 #include "win32/window.h"
+#include "win32/wgl-context.h"
 #include "test-shader.h"
 #include "reader-inl.h"
 #include "time-ticker.h"
 
+bool CheckResourceExistance() {
+  // This is only for testing.
+  util::HasLog has_log;
+  if (!util::Reader<char>::IsReadable("resources/actor.png")) {
+    util::Reader<char>::Chdir("../../");
+    if (!util::Reader<char>::IsReadable("resources/actor.png")) {
+      has_log.log << util::kLogDateTime << ": resources/actor.png or "
+                  << "../../resources.actor.png do not exist!\n";
+      return false;
+    }
+  }
+  return true;
+}
+
 INT WINAPI WinMain(HINSTANCE instance, HINSTANCE, LPSTR, int cmd_show_num) {
   core::Window window;
+  core::WGLContext wgl_context;
   core::TimeTicker ticker;
   app::SimpleShaderTest simple_shader;
 
-  window.set_instance(instance);
-
-  if (window.Init()) {
-    // This is only for testing.
-    if (!util::Reader<char>::IsReadable("resources/actor.png")) {
-      util::Reader<char>::Chdir("../../");
-      if (!util::Reader<char>::IsReadable("resources/actor.png")) {
-        window.log << util::kLogDateTime << "resources/actor.png or "
-                   << "../../resources.actor.png do not exist!\n";
-        window.Destroy();
-        return 0;
-      }
-    }
-
-    // Init test shader here
-    simple_shader.ReadResources("resources/base-vertex.vs",
-                                "resources/simple-vertex.vs",
-                                "resources/simple-fragment.vs",
-                                "resources/actor.png");
-    simple_shader.InitBuffersAndTextures();
-    simple_shader.InitShaders();
-    simple_shader.InitProgram();
-
-    glViewport(0, 0, window.width(), window.height());
-    simple_shader.set_aspect_ratio(window.width() / window.height());
-    simple_shader.set_fov(45);
+  if (!CheckResourceExistance()) {
+    return 0; 
   }
+
+  window.set_instance(instance);
+  if (!window.Init()) {
+    return 0;
+  }
+
+  wgl_context.set_pixel_format_desc();
+  if (!wgl_context.Init(window.get_hwnd())) {
+    return 0;
+  }
+  window.Show();
+
+  // Init test shader here
+  simple_shader.ReadResources("resources/base-vertex.vs",
+                              "resources/simple-vertex.vs",
+                              "resources/simple-fragment.vs",
+                              "resources/actor.png");
+  simple_shader.InitBuffersAndTextures();
+  simple_shader.InitShaders();
+  simple_shader.InitProgram();
+
+  glViewport(0, 0, window.width(), window.height());
+  simple_shader.set_aspect_ratio(window.width() / window.height());
+  simple_shader.set_fov(45);
 
   while (window.is_init()) {
     core::WindowEventType event = window.CheckForEvents();
@@ -78,12 +94,13 @@ INT WINAPI WinMain(HINSTANCE instance, HINSTANCE, LPSTR, int cmd_show_num) {
       glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
       simple_shader.Render();
-      window.Postrender();
+      wgl_context.Postrender();
       Sleep(10);
     }
   }
 
   simple_shader.Destroy();
+  wgl_context.Destroy();
   window.Destroy();
   return 0;
 }
