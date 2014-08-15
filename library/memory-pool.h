ebbafe1753314f41b5c784af3a24c0beb21222b4
ppofuk <ppofuk@gmail.com>
2014-08-15 05:32:43 +0200

    Simple memory pool.

--- library/memory-pool.h
+++ library/memory-pool.h
@@ -1,75 +1,90 @@
 // Copyright (C) 2014 Petar Pofuk. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-
-#ifndef OBSIDIAN_MEMORY_POOL_H_
-#define OBSIDIAN_MEMORY_POOL_H_
-
-#include <string.h>
-#include <stdlib.h>
-#include "static-compile-options.h"
-#include "type-traits.h"
-#include "linked-list.h"
 #include <new>
+#include <stdlib.h>
+#include "basic-types.h"
+#include "static-compile-options.h"
 
 namespace core {
-
 namespace intern {
 
-template <typename T>
-class MemoryPoolObject : public LinkNode<MemoryPoolObject<T> > {
- public:
-  friend class MemoryPool;
-  MemoryPoolObject() {}
-
- private:
-  template <typename ...Params>
-  T* New(Params... params) {
-    return new (memory_ptr_) T(params...);
-  }
-
-  T* New() {
-    return new (memory_ptr_) T;
-  }
-
-  char memory_[SizeTrait<T>::size];
+template <size_t chunk_size>
+struct PoolChunk {
+  PoolChunk()
+      : next(reinterpret_cast<PoolChunk<chunk_size>*>(
+            (char*)this + sizeof(PoolChunk*) + chunk_size)) {}
+  PoolChunk* next;
+  char memory[chunk_size];
 };
 
 }  // namespace intern
 
-
-template <typename T>
-class MemoryPool {
+template <size_t chunk_size>
+class MemoryPoolHolder {
  public:
-  MemoryPool() : pool_(0), pool_count_(0) {}
-
-  // Allocates |pool_|.
-  void AllocatePool(size_t pool_count) {
-    pool_count_ = pool_count;
-    pool_ = new intern::MemoryPoolObject[pool_count];
-
+  static void AllocateChunkRegionUnsafe(
+      size_t chunk_count = kDefaultPoolObjectCount) {
+    chunk_count_ = chunk_count;
+    chunks_ = new intern::PoolChunk<chunk_size>[chunk_count];
+    chunks_[--chunk_count].next = 0;
+    head_ = &chunks_[0];
   }
 
-  void AllocatePool() { AllocatePool(kDefaultPoolObjectCount); }
+  static void DestroyChunkRegionUnsafe() {
+    head_ = 0;
+    chunk_count_ = 0;
+    delete[] chunks_;
+  }
 
-  void DeallocatePool() {
-    if (pool_) {
-      delete[] pool_;
-      pool_ = 0;
-      pool_count_ = 0;
+  static intern::PoolChunk<chunk_size>* head() { return head_; }
+  static void append(intern::PoolChunk<chunk_size>* chunk) {
+    chunk->next = head_;
+    head_ = chunk;
+  }
+  static void set_head(intern::PoolChunk<chunk_size>* chunk) { head_ = chunk; }
+
+  static bool is_null() { return chunks_ == 0; }
+
+ private:
+  static intern::PoolChunk<chunk_size>* head_;
+  static intern::PoolChunk<chunk_size>* chunks_;
+  static size_t chunk_count_;
+};
+
+template <size_t chunk_size>
+intern::PoolChunk<chunk_size>* MemoryPoolHolder<chunk_size>::chunks_ = 0;
+
+template <size_t chunk_size>
+intern::PoolChunk<chunk_size>* MemoryPoolHolder<chunk_size>::head_ = 0;
+
+template <size_t chunk_size>
+size_t MemoryPoolHolder<chunk_size>::chunk_count_ = 0;
+
+template <size_t chunk_size,
+          typename StaticPoolType = MemoryPoolHolder<chunk_size> >
+class PooledObject {
+ public:
+  void* operator new(size_t size) {
+    if (StaticPoolType::is_null()) {
+      StaticPoolType::AllocateChunkRegionUnsafe();
     }
+
+    intern::PoolChunk<chunk_size>* chunk = StaticPoolType::head();
+    if (chunk == 0) {
+      return NULL;
+    }
+
+    StaticPoolType::set_head(chunk->next);
+    return (void*)(chunk->memory);
   }
 
-
-  // Returns the pool maximum size.
-  size_t pool_count() const { return pool_count_; }
-
- protected:
-  intern::MemoryPoolObject<T>* pool_;
-  size_t pool_count_;
-  LinkedList<intern::MemoryPoolObject<T> > free_list_;
+  void operator delete(void* ptr) {
+    intern::PoolChunk<chunk_size>* chunk =
+        reinterpret_cast<intern::PoolChunk<chunk_size>*>(
+            (char*)ptr - sizeof(intern::PoolChunk<chunk_size>*));
+    StaticPoolType::append(chunk);
+  }
 };
 
 }  // namespace core
-
-#endif  // ifndef OBSIDIAN_MEMORY_POOL_H _
