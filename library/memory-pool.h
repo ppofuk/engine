161139ce9ff16af4b613689e6c0745e78927af05
ppofuk <ppofuk@gmail.com>
2014-08-23 04:26:27 +0200

    Fixed not having once headers.

--- library/memory-pool.h
+++ library/memory-pool.h
@@ -1,6 +1,9 @@
 // Copyright (C) 2014 Petar Pofuk. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
+#ifndef OBSIDIAN_MEMORY_POOL_H_
+#define OBSIDIAN_MEMORY_POOL_H_
+
 #include <new>
 #include <stdlib.h>
 #include "basic-types.h"
@@ -87,5 +90,6 @@ class PooledObject {
   }
 };
 
-
 }  // namespace core
+
+#endif  // ifndef OBSIDIAN_MEMORY_POOL_H_
