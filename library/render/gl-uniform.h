71d1fc3c46978add4034b8194e1e61f913dde9fc
Petar Pofuk <ppofuk@gmail.com>
2017-06-08 15:47:43 +0200

    Set program without using initialization method.

--- library/render/gl-uniform.h
+++ library/render/gl-uniform.h
@@ -23,6 +23,9 @@ class GLUniform {
   GLint get_location() const { return location_; }
   void set_location(const GLint location) { location_ = location; }
   const char* get_uniform_name() const { return uniform_name_; }
+  void set_program(const render::GLProgram& program) {
+    program_ = program.get_program();
+  }
 
   // Pass method is a wraper around glUniform... function.
   void Pass(GLfloat value);
@@ -31,7 +34,7 @@ class GLUniform {
   void Pass(GLfloat* values, size_t count);
   void Pass(GLint* values, size_t count);
   void Pass(const core::Matrix4f& values, bool transpose = true);
-  void Pass(const glm::mat4x4& values, bool transpose = true); 
+  void Pass(const glm::mat4x4& values, bool transpose = true);
 
  protected:
   char uniform_name_[kUniformNameSize];
