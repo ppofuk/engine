dac2739197660e0a321c2debef556d56f6df6f85
Petar Pofuk <ppofuk@gmail.com>
2017-06-29 00:27:22 +0200

    Clear unused dependencies and remaining gyp files.

--- tools/enviroment.bat
+++
@@ -1,7 +0,0 @@
-@rem Copyright (C) 2013 Petar Pofuk. All rights reserved.
-@rem Use of this source code is governed by a BSD-style license that can be
-@rem found in the LICENSE file.
-
-@rem Add your enviroment variables here
-
-call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat"
