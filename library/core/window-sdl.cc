ba11ab82d639b34808c1e5735b68924d633556d3
Petar Pofuk <ppofuk@gmail.com>
2017-10-22 00:27:36 +0200

    Move to SDL2.

---
+++ library/core/window-sdl.cc
@@ -0,0 +1,126 @@
+#include "window-sdl.h"
+
+namespace core {
+
+void WindowSDL::DumpSDLLog() {
+  log << util::kLogDateTime << ": " << SDL_GetError() << "\n";
+}
+
+bool WindowSDL::Init(const char* title, i32 x, i32 y, i32 width, i32 height) {
+  if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER) != 0) {
+    DumpSDLLog();
+    return false;
+  }
+
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS,
+                      SDL_GL_CONTEXT_FORWARD_COMPATIBLE_FLAG);
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+  SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, gl_context_options_.double_buffer);
+  SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, gl_context_options_.depth_size);
+  SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, gl_context_options_.stencil_size);
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION,
+                      gl_context_options_.context_major_version);
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION,
+                      gl_context_options_.context_minor_version);
+
+
+  if (SDL_GetCurrentDisplayMode(0, &display_mode_) != 0) {
+    DumpSDLLog();
+    return false;
+  }
+
+  window_ = SDL_CreateWindow(title, x, y, width, height,
+                             SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE);
+  if (window_ == nullptr) {
+    DumpSDLLog();
+    return false;
+  }
+  UpdateSize(); 
+
+  gl_context_ = SDL_GL_CreateContext(window_);
+  if (gl_context_ == nullptr) {
+    DumpSDLLog();
+    return false;
+  }
+
+  glewExperimental = GL_TRUE; 
+  GLenum err = glewInit();
+  if (err != GLEW_OK) {
+    log << util::kLogDateTime << ": " << (char*)glewGetErrorString(err) << "\n";
+    Destroy();
+    return false;
+  }
+
+  char* gl_version = (char*)glGetString(GL_VERSION);
+  char* gl_vendor = (char*)glGetString(GL_VENDOR);
+  char* gl_renderer = (char*)glGetString(GL_RENDERER);
+  log << util::kLogDateTime << ": "
+      << "GL Context created\n"
+      << util::kLogDateTime << ": "
+      << "GL version: " << gl_version << "\n"
+      << util::kLogDateTime << ": "
+      << "GL vendor: " << gl_vendor << "\n"
+      << util::kLogDateTime << ": "
+      << "GL renderer: " << gl_renderer << "\n";
+  
+  is_init_ = true;
+  return true;
+}
+
+void WindowSDL::Destroy() {
+  if (gl_context_) {
+    SDL_GL_DeleteContext(gl_context_);
+    gl_context_ = nullptr; 
+  }
+
+  if (window_) {
+    SDL_DestroyWindow(window_);
+    window_ = nullptr; 
+  }
+
+  SDL_Quit();
+  is_init_ = false; 
+}
+
+void WindowSDL::Prerender() {
+  // Nothing to do here.
+}
+
+void WindowSDL::Postrender() {
+  // Swap chain
+  // TODO(ppofuk): check is_init_ maybe? 
+  SDL_GL_SwapWindow(window_); 
+}
+
+void WindowSDL::UpdateSize() {
+  SDL_GetWindowSize(window_, &width_, &height_);
+}
+
+bool WindowSDL::CheckForEvents() {
+  has_event_ = SDL_PollEvent(&event_) == 1;
+  if (has_event_) {
+    if (event_.type == SDL_WINDOWEVENT) {
+      if (event_.window.event == SDL_WINDOWEVENT_RESIZED ||
+          event_.window.event == SDL_WINDOWEVENT_SIZE_CHANGED ||
+          event_.window.event == SDL_WINDOWEVENT_EXPOSED) {
+        UpdateSize(); 
+      }
+    }
+  }
+  
+  return has_event_; 
+}
+
+void WindowSDL::Title(const char *title) {
+  SDL_SetWindowTitle(window_, title); 
+}
+
+void WindowSDL::Show(int) {
+  SDL_ShowWindow(window_); 
+}
+
+void WindowSDL::Hide() {
+  SDL_HideWindow(window_); 
+}
+
+}  // namespace core
---
+++ library/core/window-sdl.h
@@ -0,0 +1,82 @@
+#ifndef CORE_WINDOW_SDL_H_
+#define CORE_WINDOW_SDL_H_
+
+#include "GL/glew.h"
+#include "core/basic-types.h"
+#include "core/logger.h"
+
+#include <SDL.h>
+#include "core/render/gl-context-options.h"
+
+namespace core {
+
+class WindowSDL : public util::HasLog {
+ public:
+  bool Init(const char* title,
+            i32 x = 0,
+            i32 y = 0,
+            i32 width = 800,
+            i32 height = 600);
+
+  void Destroy();
+
+  // Should be called after rendering is complete.
+  void Postrender();
+
+  // Should be called before rendering.
+  void Prerender();
+
+  // Returns true if there is a pending event from internal event queue.
+  // Event is dequeued. The dequeued event can be accessed with |event()|. 
+  bool CheckForEvents();
+
+  // Maps a window to screen. Note, that this is done in |Init|.
+  // show_param is ignored (it's just for compatibility with win32 version).
+  void Show(int show_param = 0);
+
+  // Unmaps a window from screen.
+  void Hide();
+
+  // Sets window title. In X it's called store name.
+  void Title(const char* title);
+
+  inline GLContextOptions gl_context_options() { return gl_context_options_; }
+  inline void set_gl_context_options_preinit(
+      GLContextOptions gl_context_options) {
+    if (!is_init_) {
+      gl_context_options_ = gl_context_options;
+    }
+  }
+
+  SDL_Window* window() { return window_; }
+  SDL_DisplayMode display_mode() const { return display_mode_; }
+  
+  bool is_init() const { return is_init_; }
+
+  bool has_event() const { return has_event_; }
+  inline SDL_Event* event() { return &event_; }
+
+  i32 width() const { return width_; }
+  i32 height() const { return height_; }
+
+ private:
+  void DumpSDLLog();
+  // Update width_ and height_ to match window size.
+  // This is also called from |CheckForEvents|.
+  void UpdateSize(); 
+
+  i32 width_ = 0;
+  i32 height_ = 0; 
+  
+  bool is_init_ = false;
+  SDL_Window* window_ = nullptr;
+  SDL_DisplayMode display_mode_;
+  SDL_GLContext gl_context_;
+  GLContextOptions gl_context_options_;
+  SDL_Event event_;
+  bool has_event_ = false;
+};
+
+}  // namespace core
+
+#endif  // CORE_WINDOW_SDL_H_
