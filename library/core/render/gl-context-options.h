d3f6dad50b38b7f9ed844c2eb3d2132930751f62
Petar Pofuk <ppofuk@gmail.com>
2017-10-22 00:36:22 +0200

    Simple structure for OpenGL context options.

---
+++ library/core/render/gl-context-options.h
@@ -0,0 +1,19 @@
+#ifndef CORE_RENDER_GL_CONTEXT_OPTIONS_H_
+#define CORE_RENDER_GL_CONTEXT_OPTIONS_H_
+
+#include "core/basic-types.h"
+
+namespace core {
+
+struct GLContextOptions {
+  u32 context_major_version = 3;
+  u32 context_minor_version = 3;
+  u32 depth_size = 24;
+  u32 stencil_size = 8;
+  u32 double_buffer = 1; 
+};
+
+} // namespace core
+
+
+#endif // CORE_RENDER_GL_CONTEXT_OPTIONS_H_
